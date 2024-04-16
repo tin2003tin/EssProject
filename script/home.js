79680c36cbee662943ea7b1b242df7f95c893d29
Ttouch-thareewet <ttouchthareewet@gmail.com>
2024-04-16 21:56:21 +0700

    home and roomPage

---
+++ script/home.js
@@ -0,0 +1,7 @@
+document.getElementById("Submit-Button").addEventListener("click",()=>{
+    
+    var name = document.getElementById("Get-Name-Input").value;
+    
+    
+    window.location.href = "/game";
+});
